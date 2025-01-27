89cf3ad63831cbe36373624ffeb9bf6098c27942
Kyle <email@kylenoad.com>
2025-01-27 16:05:56 +0000

    3. add new endpoint of GET /api/topics

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,7 +1,19 @@
 const endpointsJson = require("../endpoints.json");
-/* Set up your test imports here */
+const app = require("../app")
+const seed = require("../db/seeds/seed")
+const request = require("supertest")
+const testData = require("../db/data/test-data/index")
+const db = require("../db/connection")
 
-/* Set up your beforeEach & afterAll functions here */
+
+beforeEach(() =>{
+  return seed(testData)
+})
+
+afterAll(() =>{
+  return db.end()
+})
+
 
 describe("GET /api", () => {
   test("200: Responds with an object detailing the documentation for each endpoint", () => {
@@ -13,3 +25,30 @@ describe("GET /api", () => {
       });
   });
 });
+
+describe("GET /api/topics", ()=>{
+  test("Should respond with an array of topic objects", ()=>{
+    return request(app)
+    .get("/api/topics")
+    .expect(200)
+    .then(({body}) =>{
+      const topics = body.topics
+      expect(topics.length).toBe(3)
+      expect(Array.isArray(topics)).toBe(true)
+      topics.forEach((topic)=>{
+        expect(topic).toMatchObject({
+          description: expect.any(String),
+          slug: expect.any(String)
+        })
+      })
+    })
+  })
+  test("Should respond with a 404 when endpoint is incorrect", ()=>{
+    return request(app)
+    .get("/api/topicsspeltincorrectly")
+    .expect(404)
+    .then((response)=>{
+      expect(response.body.error).toBe("invalid API endpoint")
+    })
+  })
+})
