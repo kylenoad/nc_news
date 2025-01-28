7e5f4f175e80839bb7b66516d5c388f0ac3af42a
Kyle <email@kylenoad.com>
2025-01-28 12:02:50 +0000

    04 - GET /api/articles/:article_id

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -43,12 +43,55 @@ describe("GET /api/topics", ()=>{
       })
     })
   })
+})
+
+describe("Testing invalid endpoints", ()=>{
   test("Should respond with a 404 when endpoint is incorrect", ()=>{
     return request(app)
     .get("/api/topicsspeltincorrectly")
     .expect(404)
     .then((response)=>{
       expect(response.body.error).toBe("invalid API endpoint")
+    })
+  })
+})
+
+describe("GET /api/articles/:article_id", ()=>{
+  test("Should respond with 200 and with an object with the correct properties when given a valid ID", ()=>{
+    return request(app)
+    .get("/api/articles/1")
+    .expect(200)
+    .then(({body})=>{
+      const article = body.article
+      expect(article).toMatchObject({
+        author: expect.any(String),
+        title: expect.any(String),
+        article_id: expect.any(Number),
+        body: expect.any(String),
+        topic: expect.any(String),
+        created_at: expect.any(String),
+        votes: expect.any(Number),
+        article_img_url: expect.any(String)
+      })
+    })
+  })
+  //errors to test
+  // - not a number - bad request 400
+  // - valid id but not matching article in db
+  test("Should respond with 404 when given article id that is correct but the article doesnt exist", ()=>{
+    return request(app)
+    .get("/api/articles/99999")
+    .expect(404)
+    .then((response)=>{
+      expect(response.body.error).toBe("Article not found")
+    })
+  })
+  test("Should respond with 400 bad request when given a query which is not a number", ()=>{
+    return request(app)
+    .get("/api/articles/a")
+    .expect(400)
+    .then((response)=>{
+      expect(response.body.error).toBe("Bad request")
     })
   })
 })
--- app.js
+++ app.js
@@ -3,19 +3,37 @@ const app = express()
 
 const { getApiEndpoints } = require("./controllers/endPointcontrollers")
 const { getTopics } = require("./controllers/topicsControllers")
+const { getArticleById } = require("./controllers/articlesControllers")
 
 app.get("/api", getApiEndpoints)
 app.get("/api/topics", getTopics)
+app.get("/api/articles/:article_id", getArticleById)
 
 
 //error handling middleware
 
-//404 ERRORS
+//invalid endpoints catch-all
 app.all("*", (req, res)=>{
     res.status(404).send( { error: "invalid API endpoint" })
 })
+//404 ERRORS - custom error - number is valid, but it is out of range
+app.use((err, req, res, next)=>{
+    if(err.msg === "Article not found"){
+        res.status(404).send({error: "Article not found"})
+    }
+    else{
+        next(err)
+    }
+})
 
 //400 ERRORS
+app.use((err, req, res, next)=>{
+    //console.log(err)
+    if(err.code === "22P02"){
+        res.status(400).send({error: "Bad request"})
+    }
+})
+
 
 //500 ERRORS
 app.use((err, req, res, next)=>{
