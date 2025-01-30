823247288a170595e0745f6610991d222476035b
Kyle <email@kylenoad.com>
2025-01-30 18:35:51 +0000

    added new property of comment count to article:id enpoint

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -113,6 +113,7 @@ describe("GET /api/articles", ()=>{
           author: expect.any(String),
           title: expect.any(String),
           article_id: expect.any(Number),
+          body: expect.any(String),
           topic: expect.any(String),
           created_at: expect.any(String),
           votes: expect.any(Number),
@@ -460,6 +461,28 @@ describe("GET /api/articles (topic query)", ()=>{
     .expect(404)
       .then(({body}) => {
         expect(body.msg).toBe("Article not found")
+    })
+  })
+})
+
+describe("GET /api/articles/:article_id (comment_count)", ()=>{
+  test("Should return with a 200 and an article object that includes comment count", ()=>{
+    return request(app)
+    .get("/api/articles/1")
+    .expect(200)
+    .then(({body})=>{
+      const article = body.article
+      expect(article).toMatchObject({
+        author: expect.any(String),
+        title: expect.any(String),
+        article_id: expect.any(Number),
+        body: expect.any(String),
+        topic: expect.any(String),
+        created_at: expect.any(String),
+        votes: expect.any(Number),
+        article_img_url: expect.any(String),
+        comment_count: 11
+      })
     })
   })
 })
