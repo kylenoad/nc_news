9ca1251e79946979a740cd24d45f5132955a5572
Kyle <email@kylenoad.com>
2025-02-21 15:30:14 +0000

    adding comment_count as valid sort query

--- models/articlesModels.js
+++ models/articlesModels.js
@@ -1,8 +1,9 @@
-const db = require("../db/connection")
+const db = require("../db/connection");
 
-const fetcharticlesById = (article_id) =>{
-    return db
-    .query(`
+const fetcharticlesById = (article_id) => {
+  return db
+    .query(
+      `
         SELECT
           articles.author, 
           articles.title, 
@@ -17,38 +18,45 @@ const fetcharticlesById = (article_id) =>{
         LEFT JOIN comments ON comments.article_id = articles.article_id
         WHERE articles.article_id = $1
         GROUP BY articles.article_id;`,
-        [article_id]
-        )
-    .then(({rows})=>{
-        if(rows.length === 0){ //=============>make this a reusable function when refactoring<==================
-            return Promise.reject({ status: 404, msg: "Article not found" })
-        }
-        else{
-        return rows[0]
-        }
-    })
-}
+      [article_id]
+    )
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        //=============>make this a reusable function when refactoring<==================
+        return Promise.reject({ status: 404, msg: "Article not found" });
+      } else {
+        return rows[0];
+      }
+    });
+};
 
 const fetchArticles = (sort_by = "created_at", order = "DESC", topic) => {
+  const validSortbyQueries = [
+    "article_id",
+    "title",
+    "topic",
+    "author",
+    "created_at",
+    "votes",
+    "comment_count",
+  ];
+  const validOrders = ["ASC", "DESC"];
 
-    const validSortbyQueries = ["article_id", "title", "topic", "author", "created_at", "votes"]
-    const validOrders = ["ASC", "DESC"]
+  if (sort_by && !validSortbyQueries.includes(sort_by)) {
+    return Promise.reject({
+      status: 400,
+      msg: "Bad request",
+    });
+  }
 
-    if (sort_by && !validSortbyQueries.includes(sort_by)) {
-        return Promise.reject({
-          status: 400,
-          msg: "Bad request"
-        })
-      }
+  if (order && !validOrders.includes(order.toUpperCase())) {
+    return Promise.reject({
+      status: 400,
+      msg: "Bad request",
+    });
+  }
 
-     if (order && !validOrders.includes(order.toUpperCase())) {
-        return Promise.reject({
-            status: 400,
-            msg: "Bad request"
-        })
-    }
-
-    let sqlQuery = `
+  let sqlQuery = `
         SELECT 
           articles.author, 
           articles.title, 
@@ -61,49 +69,47 @@ const fetchArticles = (sort_by = "created_at", order = "DESC", topic) => {
           COUNT(comments.comment_id) :: INT AS comment_count
         FROM articles
         LEFT JOIN comments ON comments.article_id = articles.article_id
+      `;
+  let queryParams = [];
+
+  if (topic) {
+    sqlQuery += ` WHERE articles.topic = $1`;
+    queryParams.push(topic);
+  }
+  sqlQuery += ` GROUP BY articles.article_id ORDER BY ${sort_by} ${order};`;
+
+  return db.query(sqlQuery, queryParams).then((result) => {
+    if (result.rows.length === 0) {
+      //=============>make this a reusable function when refactoring<==================
+      return Promise.reject({
+        status: 404,
+        msg: "Article not found",
+      });
+    }
+    return result.rows;
+  });
+};
+
+const updateVotes = (article_id, newVotes) => {
+  return db
+    .query(
       `
-      let queryParams = []
-      
-      if(topic){
-        sqlQuery += ` WHERE articles.topic = $1`
-        queryParams.push(topic)
-      }
-        sqlQuery += ` GROUP BY articles.article_id ORDER BY ${sort_by} ${order};`
-    
-    return db
-        .query(sqlQuery, queryParams)
-        .then((result) => {
-            if(result.rows.length === 0){ //=============>make this a reusable function when refactoring<==================
-                return Promise.reject({
-                    status: 404,
-                    msg: "Article not found"
-                })
-            }
-            return result.rows
-        })
-}
-
-
-const updateVotes = (article_id, newVotes) =>{
-    return db.query(
-        `
         UPDATE articles
         SET votes = votes +$2
         WHERE article_id = $1
         RETURNING *
-        `, 
-        [article_id, newVotes]
+        `,
+      [article_id, newVotes]
     )
-    .then(({rows})=>{
-        if(rows.length === 0){
-            return Promise.reject({
-                status: 404,
-                msg: "Article not found"
-            })
-        }
-        return rows[0]
-    })
-}
+    .then(({ rows }) => {
+      if (rows.length === 0) {
+        return Promise.reject({
+          status: 404,
+          msg: "Article not found",
+        });
+      }
+      return rows[0];
+    });
+};
 
-module.exports = { fetcharticlesById, fetchArticles, updateVotes}
-
+module.exports = { fetcharticlesById, fetchArticles, updateVotes };
